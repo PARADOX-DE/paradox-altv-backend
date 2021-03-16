6461ae5e29fbab5b6b93c3759cbf2d925d1476cb
Dschahannam <uwunoozles@gmail.com>
2021-03-16 23:19:55 +0100

    Add Request LoginResponse

--- PARADOX_RP/Game/Login/LoginModule.cs
+++ PARADOX_RP/Game/Login/LoginModule.cs
@@ -1,6 +1,7 @@
 ﻿using AltV.Net.Async;
 using AltV.Net.Data;
 using AltV.Net.Enums;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using PARADOX_RP.Core.Database;
 using PARADOX_RP.Core.Database.Models;
@@ -11,6 +12,7 @@ using PARADOX_RP.UI.Windows;
 using PARADOX_RP.Utils.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,14 +30,21 @@ namespace PARADOX_RP.Game.Login
         public override async void OnPlayerConnect(PXPlayer player)
         {
             WindowManager.Instance.Get<LoginWindow>().Show(player, JsonConvert.SerializeObject(new LoginWindowObject() { name = player.Name }));
-            
+
             player.Model = (uint)PedModel.FreemodeMale01;
             await player.SpawnAsync(new Position(0, 0, 72));
         }
 
-        public void RequestLoginResponse(PXPlayer player)
+        public async void RequestLoginResponse(PXPlayer player, string hashedPassword)
         {
-
+            await using (var px = new PXContext())
+            {
+                Players dbPlayer = await px.Players.FirstOrDefaultAsync(p => p.Username == player.Name);
+                if(dbPlayer.Password == hashedPassword)
+                {
+                    //LOGIN TRUE
+                }
+            }
 
             //SEND RESPONSE TO PLAYER
         }

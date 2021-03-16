d0c4ab15fbc7a70aa0f6a5dd037086fffda919ac
Dschahannam <uwunoozles@gmail.com>
2021-03-16 23:12:01 +0100

    Add Constructor

--- PARADOX_RP/Game/Administration/Models/SupportRankModel.cs
+++ PARADOX_RP/Game/Administration/Models/SupportRankModel.cs
@@ -6,6 +6,15 @@ namespace PARADOX_RP.Game.Administration.Models
 {
     public class SupportRankModel
     {
+        public SupportRankModel(int id, string name, int color_R, int color_G, int color_B)
+        {
+            Id = id;
+            Name = name;
+            Color_R = color_R;
+            Color_G = color_G;
+            Color_B = color_B;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Color_R { get; set; }

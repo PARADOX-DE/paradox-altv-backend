b4ffc622dbf6f5c1e2a67b579f39022c1b327422
Dschahannam <uwunoozles@gmail.com>
2021-03-16 23:00:17 +0100

    Add AdministrationModule

---
+++ PARADOX_RP/Game/Administration/AdministrationModule.cs
@@ -0,0 +1,26 @@
+﻿using PARADOX_RP.Core.Module;
+using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PARADOX_RP.Utils.Enums;
+
+namespace PARADOX_RP.Game.Administration
+{
+    class AdministrationModule : ModuleBase<AdministrationModule>
+    {
+        public AdministrationModule() : base("Administration") { }
+
+        public override Task<bool> OnKeyPress(PXPlayer player, KeyEnumeration key)
+        {
+            if(key == KeyEnumeration.F9)
+            {
+                //go aduty
+                return Task.FromResult(true);
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}

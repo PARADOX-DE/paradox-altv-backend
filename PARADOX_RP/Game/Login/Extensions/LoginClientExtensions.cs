079db29ffa87cd8af65c9a97c8e748647a1ffd9d
Dschahannam <uwunoozles@gmail.com>
2021-03-19 12:22:03 +0100

    Add Login ClientExtensions

---
+++ PARADOX_RP/Game/Login/Extensions/LoginClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Login.Extensions
+{
+    static class LoginClientExtensions
+    {
+        public static void PreparePlayer(this PXPlayer client) {
+            client.Emit("PreparePlayer", null);
+            //TODO: Client-Side Prepare Function
+        }
+    }
+}

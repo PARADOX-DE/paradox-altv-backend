8c9634857b3664765b87a0983e48c58b85807274
Dschahannam <uwunoozles@gmail.com>
2021-03-23 14:15:09 +0100

    Add administrative command to enter Minigame

--- PARADOX_RP/Game/MiniGames/MinigameModule.cs
+++ PARADOX_RP/Game/MiniGames/MinigameModule.cs
@@ -1,4 +1,6 @@
-﻿using PARADOX_RP.Core.Factories;
+﻿using AltV.Net.Resources.Chat.Api;
+using PARADOX_RP.Core.Factories;
+using PARADOX_RP.Core.Extensions;
 using PARADOX_RP.Core.Module;
 using PARADOX_RP.Game.Lobby;
 using PARADOX_RP.Game.MiniGames.Interfaces;
@@ -8,19 +10,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace PARADOX_RP.Game.MiniGames
 {
     class MinigameModule : ModuleBase<MinigameModule>
     {
         private readonly IEnumerable<IMinigame> _minigames;
-        public MinigameModule(IEnumerable<IMinigame> minigames) : base("Minigame") {
+        public MinigameModule(IEnumerable<IMinigame> minigames) : base("Minigame")
+        {
             _minigames = minigames;
         }
 
         public void ChooseMinigame(PXPlayer player, MinigameTypes minigame)
         {
-            if(player.Minigame != MinigameTypes.NONE)
+            if (player.Minigame != MinigameTypes.NONE)
             {
                 player.SendNotification("Minigame", "Du bist bereits in einem Minigame.", NotificationTypes.ERROR);
                 return;
@@ -35,6 +39,16 @@ namespace PARADOX_RP.Game.MiniGames
 
             player.Minigame = minigame;
             player.Dimension = LobbyModule.Instance.GetDimensionByLobby(lobby);
+        }
+
+        [Command("minigame")]
+        public void enterMinigameCommand(PXPlayer player, string minigameModule)
+        {
+            MinigameTypes _minigameType = Enum.Parse<MinigameTypes>(minigameModule);
+            IMinigame minigame = _minigames.FirstOrDefault(i => i.MinigameType == _minigameType);
+
+            player.Minigame = _minigameType;
+            minigame.EnteredMinigame(player);
         }
     }
 }

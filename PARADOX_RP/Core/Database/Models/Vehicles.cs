35136368befe20654d2c1c8bd82420bc54bfa584
Dschahannam <uwunoozles@gmail.com>
2021-03-25 08:39:40 +0100

    Add Vehicle Database-Struct

---
+++ PARADOX_RP/Core/Database/Models/Vehicles.cs
@@ -0,0 +1,23 @@
+﻿using AltV.Net.Elements.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Core.Database.Models
+{
+    class Vehicles
+    {
+        public Vehicles(int playerId, string vehicleModel)
+        {
+            PlayerId = playerId;
+            VehicleModel = vehicleModel;
+        }
+
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public string VehicleModel { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual Players Player { get; set; }
+    }
+}

68e6552685d07cf33f918e69c0d58658b8cf5cb1
Dschahannam <uwunoozles@gmail.com>
2021-03-25 09:07:12 +0100

    dependency injection

--- PARADOX_RP/Game/Team/TeamModule.cs
+++ PARADOX_RP/Game/Team/TeamModule.cs
@@ -1,5 +1,6 @@
 ﻿using PARADOX_RP.Core.Database.Models;
 using PARADOX_RP.Core.Module;
+using PARADOX_RP.Handlers.Team;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,8 +11,11 @@ namespace PARADOX_RP.Game.Team
     class TeamModule : ModuleBase<TeamModule>
     {
         public readonly Dictionary<int, Teams> TeamList;
+        private readonly TeamHandler _teamHandler;
 
-        public TeamModule() : base("Team") { }
+        public TeamModule(TeamHandler teamHandler) : base("Team") {
+            _teamHandler = teamHandler;
+        }
 
         
     }

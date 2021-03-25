c4535730c1730bbe283c079a9a6d3c9ad8a73d7f
Dschahannam <uwunoozles@gmail.com>
2021-03-25 08:52:36 +0100

    Add Teams Database-Struct

---
+++ PARADOX_RP/Core/Database/Models/Teams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Core.Database.Models
+{
+    public enum TeamTypes
+    {
+        NEUTRAL,
+        STATE,
+        BAD
+    }
+
+    class Teams
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public TeamTypes TeamType { get; set; }
+    }
+}

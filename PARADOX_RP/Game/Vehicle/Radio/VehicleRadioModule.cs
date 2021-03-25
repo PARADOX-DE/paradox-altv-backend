d1a5bc7528f01d71dc081c35f9cfb32cb0a0faaf
Dschahannam <uwunoozles@gmail.com>
2021-03-25 12:38:05 +0100

    Add VehicleSeat Enumeration

--- PARADOX_RP/Game/Vehicle/Radio/VehicleRadioModule.cs
+++ PARADOX_RP/Game/Vehicle/Radio/VehicleRadioModule.cs
@@ -3,6 +3,7 @@ using AltV.Net.Elements.Entities;
 using PARADOX_RP.Core.Factories;
 using PARADOX_RP.Core.Module;
 using PARADOX_RP.Utils;
+using PARADOX_RP.Utils.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,7 +13,20 @@ namespace PARADOX_RP.Game.Vehicle.Radio
 {
     class VehicleRadioModule : ModuleBase<VehicleRadioModule>
     {
-        public VehicleRadioModule() : base("VehicleRadio") { }
+        public VehicleRadioModule() : base("VehicleRadio") {
+            AltAsync.OnClient<PXPlayer>("EnableVehicleRadio", EnableVehicleRadio);
+            AltAsync.OnClient<PXPlayer>("DisableVehicleRadio", DisableVehicleRadio);
+        }
+
+        private async void DisableVehicleRadio(PXPlayer obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void EnableVehicleRadio(PXPlayer player)
+        {
+
+        }
 
         public override async Task OnPlayerEnterVehicle(IVehicle v, IPlayer p, byte seat)
         {

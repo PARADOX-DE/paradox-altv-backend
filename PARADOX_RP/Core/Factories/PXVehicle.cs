885c202911b27dec4caaf5c4be6d0bb3ca57ef5b
Dschahannam <uwunoozles@gmail.com>
2021-03-25 08:32:50 +0100

    Add Vehicle Class for EF

---
+++ PARADOX_RP/Core/Factories/PXVehicle.cs
@@ -0,0 +1,18 @@
+﻿using AltV.Net.Elements.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Core.Factories
+{
+    public class PXVehicle : Vehicle
+    {
+        public int SqlId { get; set; }
+
+        internal PXVehicle(IntPtr nativePointer, ushort id) : base(nativePointer, id)
+        {
+            SqlId = -1;
+        }
+
+    }
+}

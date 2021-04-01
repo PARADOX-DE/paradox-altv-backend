a8545dec2894817bb2131b33c9270be7a39e185a
Dschahannam <uwunoozles@gmail.com>
2021-04-01 23:09:53 +0200

    Add RequestLoginResponse Listener

--- PARADOX_RP/Game/Login/LoginModule.cs
+++ PARADOX_RP/Game/Login/LoginModule.cs
@@ -33,6 +33,8 @@ namespace PARADOX_RP.Game.Login
         public LoginModule(ILoginHandler loginHandler) : base("Login")
         {
             _loginHandler = loginHandler;
+
+            AltAsync.OnClient<PXPlayer, string>("RequestLoginResponse", RequestLoginResponse);
         }
 
         public override void OnModuleLoad()
@@ -58,7 +60,7 @@ namespace PARADOX_RP.Game.Login
             WindowManager.Instance.Get<LoginWindow>().Show(player, JsonConvert.SerializeObject(new LoginWindowObject() { name = player.Name }));
         }
 
-        public async Task RequestLoginResponse(PXPlayer player, string hashedPassword)
+        public async void RequestLoginResponse(PXPlayer player, string hashedPassword)
         {
             if (player.LoggedIn) return;
 

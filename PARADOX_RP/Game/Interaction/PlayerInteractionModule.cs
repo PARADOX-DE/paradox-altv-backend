c85aab07ab60146b017ad994d4c5f55a48a6e857
Dschahannam <uwunoozles@gmail.com>
2021-04-02 16:25:20 +0200

    add class for playerinteraction

---
+++ PARADOX_RP/Game/Interaction/PlayerInteractionModule.cs
@@ -0,0 +1,15 @@
+﻿using PARADOX_RP.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Interaction
+{
+    class PlayerInteractionModule : ModuleBase<PlayerInteractionModule>
+    {
+        public PlayerInteractionModule() : base("PlayerInteraction")
+        {
+
+        }
+    }
+}

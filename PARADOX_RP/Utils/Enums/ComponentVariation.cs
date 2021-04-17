7a4fc66c51ac6e290ef751c7e04e713b7b848620
Dschahannam <uwunoozles@gmail.com>
2021-04-17 07:36:54 +0200

    Add ComponentVariation enumeration

--- PARADOX_RP/Utils/Enums/ComponentVariation.cs
+++ PARADOX_RP/Utils/Enums/ComponentVariation.cs
@@ -6,6 +6,17 @@ namespace PARADOX_RP.Utils.Enums
 {
     public enum ComponentVariation
     {
-        
+        HEAD,
+        MASKS,
+        HAIR,
+        TORSO,
+        LEGS,
+        BAGS,
+        SHOES,
+        ACCESSORIES,
+        UNDERSHIRT,
+        ARMOR,
+        DECALS,
+        TOP
     }
 }

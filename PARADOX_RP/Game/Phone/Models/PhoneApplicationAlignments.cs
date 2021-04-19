e5e2f73bf789d78ef8ed60b1166f91954741c12c
Dschahannam <uwunoozles@gmail.com>
2021-04-19 15:47:11 +0200

    Add PhoneApplicationModel and PhoneApplicationAlignment enumeration

---
+++ PARADOX_RP/Game/Phone/Models/PhoneApplicationAlignments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Phone.Models
+{
+    public enum PhoneApplicationAlignments
+    {
+        TOP,
+        BOTTOM
+    }
+}
---
+++ PARADOX_RP/Game/Phone/PhoneModule.cs
@@ -0,0 +1,17 @@
+﻿using PARADOX_RP.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Phone
+{
+    class PhoneModule : ModuleBase<PhoneModule>
+    {
+        public PhoneModule() : base("Phone")
+        {
+
+        }
+
+        private Dictionary<int, >
+    }
+}

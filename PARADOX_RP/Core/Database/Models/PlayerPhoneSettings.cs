ad1a86c33c4bb0b36cca44a0de11a55ac0ff6ecd
Dschahannam <uwunoozles@gmail.com>
2021-04-22 09:37:07 +0200

    Add PlayerPhoneSettings

---
+++ PARADOX_RP/Core/Database/Models/PlayerPhoneSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PARADOX_RP.Core.Database.Models
+{
+    [Table("player_phone_settings")]
+    public class PlayerPhoneSettings
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public string ProfileImageURL { get; set; }
+
+        public virtual Players Player { get; set; }
+    }
+}

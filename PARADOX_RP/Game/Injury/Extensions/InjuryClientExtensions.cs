8e3951d69a335f4a021faf742df9cd22cc6c5536
Dschahannam <uwunoozles@gmail.com>
2021-04-29 23:01:54 +0200

    Add functions to revive player

---
+++ PARADOX_RP/Game/Injury/Extensions/InjuryClientExtensions.cs
@@ -0,0 +1,36 @@
+﻿using PARADOX_RP.Core.Database;
+using PARADOX_RP.Core.Database.Models;
+using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARADOX_RP.Game.Injury.Extensions
+{
+    public static class InjuryClientExtensions
+    {
+        public static Task Revive(this PXPlayer player, bool spawnAtCurrentPosition = true, bool keepMoney = true, bool keepInventory = true)
+        {
+            if (!player.Exists) return Task.FromResult(false);
+            if (!player.IsValid()) return Task.FromResult(false);
+
+            if (spawnAtCurrentPosition) player.Spawn(player.Position, 0);
+            if (!keepMoney) player.Money = 0;
+            if (!keepInventory) { /* TODO: clear inventory */ }
+
+            player.Injured = false;
+
+            using(var px = new PXContext())
+            {
+                Players dbPlayer = px.Players.Find(player.SqlId);
+                if (dbPlayer == null) return Task.FromResult(false);
+
+                //TODO: clear db injurystate
+                px.SaveChanges();
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- PARADOX_RP/Game/Injury/InjuryModule.cs
+++ PARADOX_RP/Game/Injury/InjuryModule.cs
@@ -1,5 +1,6 @@
 ﻿using PARADOX_RP.Controllers.Event.Interface;
 using PARADOX_RP.Controllers.Team.Interface;
+using PARADOX_RP.Core.Factories;
 using PARADOX_RP.Core.Module;
 using System;
 using System.Collections.Generic;
@@ -7,15 +8,45 @@ using System.Text;
 
 namespace PARADOX_RP.Game.Injury
 {
+    public enum InjuryTypes
+    {
+        OTHER,
+        KNOCK_OUT,
+        CUT,
+        SHOT
+    }
+
     public class InjuryModule : ModuleBase<InjuryModule>
     {
         private IEventController _eventController;
         private ITeamController _teamController;
+
+        //TODO: add dbInjury Model
+        private readonly Dictionary<uint, object> _injuries;
+
         public InjuryModule(IEventController eventController, ITeamController teamController) : base("Injury")
         {
             _eventController = eventController;
             _teamController = teamController;
+
+            _injuries = new Dictionary<uint, object>();
         }
 
+
+        public override void OnPlayerDeath(PXPlayer player, PXPlayer killer, uint reason)
+        {
+            //InjuryModule only for injuries in dimension 0
+            if (player.Dimension != 0) return;
+
+            if (_injuries.TryGetValue(reason, out object injuryType))
+            {
+
+            }
+            else
+            {
+                //Injury not found in database
+                player.
+            }
+        }
     }
 }

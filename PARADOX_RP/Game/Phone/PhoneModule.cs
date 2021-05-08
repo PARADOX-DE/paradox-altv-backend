e82104daad2e2673ea0831250c7352f6b31abf02
Dschahannam <uwunoozles@gmail.com>
2021-05-08 17:15:32 +0200

    Add Phone

--- PARADOX_RP/Game/Phone/PhoneModule.cs
+++ PARADOX_RP/Game/Phone/PhoneModule.cs
@@ -1,8 +1,12 @@
-﻿using PARADOX_RP.Core.Database;
+﻿using PARADOX_RP.Controllers.Event.Interface;
+using PARADOX_RP.Core.Database;
 using PARADOX_RP.Core.Extensions;
+using PARADOX_RP.Core.Factories;
 using PARADOX_RP.Core.Module;
 using PARADOX_RP.Game.Phone.Interfaces;
 using PARADOX_RP.Game.Phone.Models;
+using PARADOX_RP.UI;
+using PARADOX_RP.UI.Windows.Phone;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,13 +16,22 @@ namespace PARADOX_RP.Game.Phone
     class PhoneModule : ModuleBase<PhoneModule>
     {
         private IEnumerable<PhoneApplicationModel> _phoneApplications;
-        public PhoneModule(PXContext pxContext, IEnumerable<IPhoneApplication> phoneApplications) : base("Phone")
+        public PhoneModule(PXContext pxContext, IEventController eventController, IEnumerable<IPhoneApplication> phoneApplications) : base("Phone")
         {
             _phoneApplications = LoadDatabaseTable<PhoneApplicationModel>(null, (p) =>
             {
                 //todo: db table yk
             });
+
+            eventController.OnClient<PXPlayer>("OpenPhone", OpenPhone);
         }
 
+        private void OpenPhone(PXPlayer player)
+        {
+            if (!player.CanInteract() || !player.IsValid()) return;
+            if (!player.HasPhone) return;
+
+            WindowManager.Instance.Get<PhoneWindow>().Show(player);
+        }
     }
 }
---
+++ PARADOX_RP/UI/Windows/Phone/PhoneWindow.cs
@@ -0,0 +1,12 @@
+﻿using PARADOX_RP.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.UI.Windows.Phone
+{
+    class PhoneWindow : Window
+    {
+        public PhoneWindow() : base("Phone") { }
+    }
+}

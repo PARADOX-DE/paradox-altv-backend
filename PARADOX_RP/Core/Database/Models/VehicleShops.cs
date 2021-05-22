44f01474e6a909d6c86cc88bcb6c6d59864a3d1b
Dschahannam <uwunoozles@gmail.com>
2021-05-22 18:01:19 +0200

    Add database struct VehicleShops

---
+++ PARADOX_RP/Core/Database/Models/VehicleShops.cs
@@ -0,0 +1,23 @@
+﻿using AltV.Net.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PARADOX_RP.Core.Database.Models
+{
+    [Table("vehicle_shops")]
+    public partial class VehicleShops
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Position_X { get; set; }
+        public float Position_Y { get; set; }
+        public float Position_Z { get; set; }
+    }
+
+    public partial class VehicleShops
+    {
+        public Position Position => new Position(Position_X, Position_Y, Position_Z);
+    }
+}

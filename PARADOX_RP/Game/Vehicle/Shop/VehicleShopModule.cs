cef18f7243cb2479a144b3a74f4fd5c182df2884
Dschahannam <uwunoozles@gmail.com>
2021-05-22 17:57:26 +0200

    Add Base for VehicleShopModule

---
+++ PARADOX_RP/Game/Vehicle/Shop/VehicleShopModule.cs
@@ -0,0 +1,15 @@
+﻿using PARADOX_RP.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Vehicle.Shop
+{
+    class VehicleShopModule : ModuleBase<VehicleShopModule>
+    {
+        public VehicleShopModule() : base("VehicleShop")
+        {
+
+        }
+    }
+}

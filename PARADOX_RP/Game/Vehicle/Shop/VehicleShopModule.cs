53c96dcf13d56c4d6bed334bcaa415c82660bb28
Dschahannam <uwunoozles@gmail.com>
2021-05-22 17:59:13 +0200

    [VehShop] Register KeyPress

--- PARADOX_RP/Game/Vehicle/Shop/VehicleShopModule.cs
+++ PARADOX_RP/Game/Vehicle/Shop/VehicleShopModule.cs
@@ -1,7 +1,10 @@
-﻿using PARADOX_RP.Core.Module;
+﻿using PARADOX_RP.Core.Factories;
+using PARADOX_RP.Core.Module;
+using PARADOX_RP.Utils.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PARADOX_RP.Game.Vehicle.Shop
 {
@@ -10,6 +13,15 @@ namespace PARADOX_RP.Game.Vehicle.Shop
         public VehicleShopModule() : base("VehicleShop")
         {
 
+        }
+
+        public override Task<bool> OnKeyPress(PXPlayer player, KeyEnumeration key)
+        {
+            if (key != KeyEnumeration.E) return Task.FromResult(false);
+
+            
+
+            return Task.FromResult(false);
         }
     }
 }

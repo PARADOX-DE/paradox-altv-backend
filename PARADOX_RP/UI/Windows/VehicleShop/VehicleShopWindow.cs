685ac7474d314e6558bbdbe45df3562f4af9d94a
Dschahannam <uwunoozles@gmail.com>
2021-05-22 17:58:27 +0200

    Add Window VehicleShop

---
+++ PARADOX_RP/UI/Windows/VehicleShop/VehicleShopWindow.cs
@@ -0,0 +1,12 @@
+﻿using PARADOX_RP.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.UI.Windows.CarShop
+{
+    class VehicleShopWindow : Window
+    {
+        public VehicleShopWindow() : base("VehicleShop") { }
+    }
+}

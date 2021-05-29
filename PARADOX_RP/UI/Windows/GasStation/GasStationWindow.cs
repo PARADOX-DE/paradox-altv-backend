95af67526d9d39cdb86baa6368e38f414821ad22
BraceLIVE <businessbrace2801@gmail.com>
2021-05-30 00:23:32 +0200

    GasStationWindow

---
+++ PARADOX_RP/UI/Windows/GasStation/GasStationWindow.cs
@@ -0,0 +1,14 @@
+﻿using AltV.Net;
+using PARADOX_RP.Core.Database.Models;
+using PARADOX_RP.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.UI.Windows
+{
+    class GasStationWindow : Window
+    {
+        public GasStationWindow() : base("GasStation") { }
+    }
+}

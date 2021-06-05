316fd4ead5fc490baffaf7b60536c85ce5e974ad
Dschahannam <uwunoozles@gmail.com>
2021-06-05 16:17:15 +0200

    Started working on Team Garages

---
+++ PARADOX_RP/Game/Garage/Team/TeamGarageModule.cs
@@ -0,0 +1,17 @@
+﻿using PARADOX_RP.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Garage.Team
+{
+    class TeamGarageModule : ModuleBase<TeamGarageModule>
+    {
+        public TeamGarageModule() : base("TeamGarage")
+        {
+
+        }
+
+
+    }
+}

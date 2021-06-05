0053e564f1dbc8680eba30b4a48159db3123d9e3
Dschahannam <uwunoozles@gmail.com>
2021-06-05 16:28:29 +0200

    Add RotationExtensions

---
+++ PARADOX_RP/Core/Extensions/RotationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Core.Extensions
+{
+    internal static class RotationExtensions
+    {
+        public static float YawToDegree(this float Yaw)
+        {
+            return (float)((float)Yaw * 180 / Math.PI);
+        } 
+    }
+}

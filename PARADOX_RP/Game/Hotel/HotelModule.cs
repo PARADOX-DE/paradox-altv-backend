5d39a0e3c1cf6a131c9fae4326c6c8dd736c11ec
Dschahannam <uwunoozles@gmail.com>
2021-06-08 01:58:14 +0200

    Create Hotel Blip on ModuleLoad

--- PARADOX_RP/Game/Hotel/HotelModule.cs
+++ PARADOX_RP/Game/Hotel/HotelModule.cs
@@ -1,15 +1,32 @@
-﻿using PARADOX_RP.Core.Module;
+﻿using AltV.Net.Data;
+using PARADOX_RP.Core.Events;
+using PARADOX_RP.Core.Factories;
+using PARADOX_RP.Core.Module;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PARADOX_RP.Game.Hotel
 {
-    class HotelModule : ModuleBase<HotelModule>
+    class HotelModule : ModuleBase<HotelModule>, IEventModuleLoad, IEventPlayerConnect
     {
+        public readonly string _hotelName = "Crastenburg Hotel";
+        public readonly Position _blipPosition = new Position(-1237.5428f, -189.53406f, 41.61389f);
+
         public HotelModule() : base("Hotel")
         {
 
+        }
+
+        public void OnModuleLoad()
+        {
+
+        }
+
+        public void OnPlayerConnect(PXPlayer player)
+        {
+            /* CREATE HOTEL BLIP */
+            player.AddBlips(_hotelName, _blipPosition, 475, 56, 1, true);
         }
     }
 }

533ea34d283c48b7780eadd9bc49ca329bd698d7
Dschahannam <uwunoozles@gmail.com>
2021-06-07 13:45:28 +0200

    Add WeaponChange Event

---
+++ PARADOX_RP/Core/Events/IEventPlayerWeaponChange.cs
@@ -0,0 +1,13 @@
+﻿using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Core.Events
+{
+    interface IEventPlayerWeaponChange
+    {
+        bool Enabled { get; }
+        void OnPlayerWeaponChange(PXPlayer player, uint oldWeapon, uint newWeapon);
+    }
+}
---
+++ PARADOX_RP/Game/Weapon/WeaponModule.cs
@@ -0,0 +1,17 @@
+﻿using AltV.Net;
+using AltV.Net.Events;
+using PARADOX_RP.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARADOX_RP.Game.Weapon
+{
+    class WeaponModule : ModuleBase<WeaponModule>
+    {
+        public WeaponModule() : base("Weapon")
+        {
+
+        }
+    }
+}

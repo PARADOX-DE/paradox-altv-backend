83915529f214d7641691689c158d0e6398226fc5
Dschahannam <uwunoozles@gmail.com>
2021-06-07 13:17:25 +0200

    Add AddWeapon to WeaponController

---
+++ PARADOX_RP/Controllers/Weapon/Interface/IWeaponController.cs
@@ -0,0 +1,15 @@
+﻿using AltV.Net.Data;
+using AltV.Net.Enums;
+using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARADOX_RP.Controllers.Weapon.Interface
+{
+    interface IWeaponController
+    {
+        Task AddWeapon(PXPlayer player, WeaponModel weapon);
+    }
+}
---
+++ PARADOX_RP/Controllers/Weapon/WeaponController.cs
@@ -0,0 +1,19 @@
+﻿using AltV.Net.Data;
+using AltV.Net.Enums;
+using PARADOX_RP.Controllers.Weapon.Interface;
+using PARADOX_RP.Core.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARADOX_RP.Controllers.Weapon
+{
+    class WeaponController : IWeaponController
+    {
+        public async Task AddWeapon(PXPlayer player, WeaponModel weapon)
+        {
+            player.GiveWeapon((uint)weapon, 0, true);
+        }
+    }
+}

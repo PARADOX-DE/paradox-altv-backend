dafbd110d646b034f9a121d97faf8372a0d46b38
Dschahannam <uwunoozles@gmail.com>
2021-06-12 18:23:31 +0200

    Register events for communication with anticheat

--- PARADOX_RP/Game/Anticheat/AnticheatModule.cs
+++ PARADOX_RP/Game/Anticheat/AnticheatModule.cs
@@ -1,4 +1,5 @@
 ﻿using PARADOX_RP.Controllers.Event.Interface;
+using PARADOX_RP.Core.Factories;
 using PARADOX_RP.Core.Module;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,18 @@ namespace PARADOX_RP.Game.Anticheat
     {
         public AnticheatModule(IEventController eventController) : base("Anticheat")
         {
-            
+            eventController.OnClient<PXPlayer>("LoadAnticheat", LoadAnticheat);
+            eventController.OnClient<PXPlayer>("FlagAnticheat", FlagAnticheat);
+        }
+
+        private void FlagAnticheat(PXPlayer player)
+        {
+            player.SendNotification("Anticheat", "Cheat Injection detected.", NotificationTypes.SUCCESS);
+        }
+
+        private void LoadAnticheat(PXPlayer player)
+        {
+            player.SendNotification("Anticheat", "Loaded successful.", NotificationTypes.SUCCESS);
         }
     }
 }

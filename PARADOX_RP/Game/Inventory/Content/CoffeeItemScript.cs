28fc584c3eeab7894a0fc199808d4f548891dcde
zeroday-z <denigt42@gmail.com>
2021-07-08 12:50:33 -0700

    time for a coffee.

---
+++ PARADOX_RP/Game/Inventory/Content/CoffeeItemScript.cs
@@ -0,0 +1,35 @@
+﻿using AltV.Net.Async;
+using AltV.Net.Async.Elements.Refs;
+using AltV.Net.Data;
+using EntityStreamer;
+using PARADOX_RP.Core.Factories;
+using PARADOX_RP.Game.Inventory.Interfaces;
+using PARADOX_RP.Game.Misc.Progressbar.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARADOX_RP.Game.Inventory.Content
+{
+    class CoffeeItemScript : IItemScript
+    {
+        public string ScriptName => "coffee_itemscript";
+
+        public async Task<bool> UseItem(PXPlayer player)
+        {
+
+         await player.PlayAnimation("amb@world_human_drinking@coffee@male@idle_a", "idle_a");
+
+
+         player.SendNotification("Kaffee", "Du hast einen leckeren Kaffee getrunken!", NotificationTypes.SUCCESS);
+           
+
+         if (new AsyncPlayerRef(player).Exists)
+                await player.StopAnimation();
+
+         return await Task.FromResult(true);
+        }
+    }
+}
